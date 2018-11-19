23c1f58a0f109deaabfa6a43e1139d31961f2e9f
vishuvish <vishvishu14@gmail.com>
2018-11-19 19:49:04 +0530

    Update to generate the reports for last 5mins

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,9 @@
 import schedule
 import time
 from prettytable import PrettyTable
-import json
 import ast
 from collections import Counter
 import pandas as pd
-# from .tweets import get_tweets
 
 def get_unique_words(count):
     dfr = pd.DataFrame.from_dict(count, orient='index')
@@ -13,32 +11,39 @@ def get_unique_words(count):
 
 
 def job():
-    print("Doing")
+    print("Generating Reports:\n")
+
+    current_time = time.time()
+
     columns1 = ['S.No.', 'Handle Name', 'Tweets Count']
 
     table1 = PrettyTable(columns1)
 
-    with open('tweetDB4.csv') as f:
+    with open('tweetDB4.csv', 'r', encoding='utf-8') as f:
         i = 0
         words, links = [], []
         for line in f:
-            if i==10:
-                print("User Report")
-                print(table1)
-                print("Content Report")
-                count_words = Counter(words)
-                print(get_unique_words(count_words))
-                print("Total Number of Unique Words", len(words))
-                print("Link Report")
-                print("Total Number of Links", len(links))
-                break
 
             data = ast.literal_eval(line)
-            table1.add_row([i+1, data["user"], data["status_count"]])
-            words+=((data["unique_words"]).split(" "))
-            links+=((data["links"][1:-1]).aplit(","))
-            i+=1
 
+            # print(current_time, data["timestamp"], (current_time - data["timestamp"]))
+            if 0 <= (current_time - data["timestamp"]) <= 300:
+                table1.add_row([i+1, data["user"], data["status_count"]])
+                words+=((data["unique_words"]).split(" "))
+                links+=((data["links"][1:-1]).split(","))
+                i+=1
+            elif data["timestamp"] > current_time or not data:
+                print("User Report:")
+                print(table1)
+                print("\nContent Report:")
+                count_words = Counter(words)
+                print(get_unique_words(count_words))
+                print("\nTotal Number of Unique Words- ", len(words))
+                print("\nLink Report:")
+                print("Total Number of Links- ", len(links))
+                break
+            else:
+                continue
 
 schedule.every(1).minutes.do(job)
 
